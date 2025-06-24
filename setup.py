45b0973f000fffc7c1d50feaddcdd9348fb90af2
Vickey-vijay <perryvickey421@gmail.com>
2025-06-25 00:21:19 +0530

    changes done

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A simple calculator library for basic arithmetic operations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/calclib",
+    url="https://github.com/Vickey-vijay/calclib/",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
